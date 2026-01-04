6f7dd2aa49c921c2d0d38d68ccb3f37acb3fad4f
antarnath <antar.ch.nath@gmail.com>
2026-01-04 21:44:14 +0600

    complete model training and connect MLFlow

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from src.logger import logging
 from src.exception import CustomException
+from src.components.model_trainer import ModelTrainer
 from dataclasses import dataclass
 from sklearn.model_selection import train_test_split
 from src.components.data_transformation import DataTransformation
@@ -52,3 +53,5 @@ if __name__ == "__main__":
   data_transformation = DataTransformation()
   train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
   
+  model_trainer = ModelTrainer()
+  print(model_trainer.initiate_model_trainer(train_arr, test_arr))
